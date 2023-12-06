9e73119f5d04600ab46eab869febb3d66587461b
David Stocking <david.stocking@stryker.com>
2023-12-06 16:11:25 -0500

    refactor: Clean up processing list

--- 2023/Day1.hs
+++ 2023/Day1.hs
@@ -15,28 +15,31 @@ sumCalibration :: String -> String
 sumCalibration = show . sum . map (firstAndLast . replaceNumbers) . lines . unpack . toLower . pack
 
 replaceNumbers :: String -> [Int]
-replaceNumbers ('0':rest) = 0 : replaceNumbers rest
-replaceNumbers ('1':rest) = 1 : replaceNumbers rest
-replaceNumbers ('2':rest) = 2 : replaceNumbers rest
-replaceNumbers ('3':rest) = 3 : replaceNumbers rest
-replaceNumbers ('4':rest) = 4 : replaceNumbers rest
-replaceNumbers ('5':rest) = 5 : replaceNumbers rest
-replaceNumbers ('6':rest) = 6 : replaceNumbers rest
-replaceNumbers ('7':rest) = 7 : replaceNumbers rest
-replaceNumbers ('8':rest) = 8 : replaceNumbers rest
-replaceNumbers ('9':rest) = 9 : replaceNumbers rest
-replaceNumbers a@('z':'e':'r':'o':rest) = 0 : replaceNumbers (drop 1 a)
-replaceNumbers a@('o':'n':'e':rest) = 1 : replaceNumbers (drop 1 a)
-replaceNumbers a@('t':'w':'o':rest) = 2 : replaceNumbers (drop 1 a)
-replaceNumbers a@('t':'h':'r':'e':'e':rest) = 3 : replaceNumbers (drop 1 a)
-replaceNumbers a@('f':'o':'u':'r':rest) = 4 : replaceNumbers (drop 1 a)
-replaceNumbers a@('f':'i':'v':'e':rest) = 5 : replaceNumbers (drop 1 a)
-replaceNumbers a@('s':'i':'x':rest) = 6 : replaceNumbers (drop 1 a)
-replaceNumbers a@('s':'e':'v':'e':'n':rest) = 7 : replaceNumbers (drop 1 a)
-replaceNumbers a@('e':'i':'g':'h':'t':rest) = 8 : replaceNumbers (drop 1 a)
-replaceNumbers a@('n':'i':'n':'e':rest) = 9 : replaceNumbers (drop 1 a)
-replaceNumbers (_:rest) = replaceNumbers rest
 replaceNumbers [] = []
+replaceNumbers str = digit str ++ replaceNumbers (drop 1 str)
+
+digit :: String -> [Int]
+digit ('0':rest) = [0]
+digit ('1':rest) = [1]
+digit ('2':rest) = [2]
+digit ('3':rest) = [3]
+digit ('4':rest) = [4]
+digit ('5':rest) = [5]
+digit ('6':rest) = [6]
+digit ('7':rest) = [7]
+digit ('8':rest) = [8]
+digit ('9':rest) = [9]
+digit ('z':'e':'r':'o':rest) = [0]
+digit ('o':'n':'e':rest) = [1]
+digit ('t':'w':'o':rest) = [2]
+digit ('t':'h':'r':'e':'e':rest) = [3]
+digit ('f':'o':'u':'r':rest) = [4]
+digit ('f':'i':'v':'e':rest) = [5]
+digit ('s':'i':'x':rest) = [6]
+digit ('s':'e':'v':'e':'n':rest) = [7]
+digit ('e':'i':'g':'h':'t':rest) = [8]
+digit ('n':'i':'n':'e':rest) = [9]
+digit _ = []
 
 firstAndLast :: [Int] -> Int
 firstAndLast list = head list * 10 + last list
